dd29b5d9833a20205136ebe873ad9968499e562e
Andrew Cosmic <acosmic@liveperson.com>
2024-04-30 14:41:10 -0700

    added image slider and color guesser

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import App from './App.tsx'
 import Home from './Home.tsx'
 import Search from './SynonymsSearch/SynonymsSearch'
 import Post from './Posts/DisplayPosts'
+import Guess from './GuessColor/Guess'
 import MemoryBoard from './MemoryBoard/MemoryBoard'
 import Todo from './Todo/Todo'
+import ImageSlider from './ImageSlider/ImageSlider'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Tree from './Tree/Tree.tsx'
 const router = createBrowserRouter([
   {
     element: <App />,
@@ -34,6 +37,14 @@ const router = createBrowserRouter([
       {
         path: "/todo",
         element: <Todo />,
+      },
+      {
+        path: "/guess",
+        element: <Guess />,
+      },
+      {
+        path: "/slide",
+        element: <ImageSlider />,
       },
     ],
   },
