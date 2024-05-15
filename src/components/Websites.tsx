66ebbf579c7824aec56f72a583b2840a55b6ffd6
Andrew Cosmic <acdickin@gmail.com>
2024-05-15 14:26:51 -0700

    added knowledge center

--- src/components/Websites.tsx
+++ src/components/Websites.tsx
@@ -1,19 +1,43 @@
 import exerciseBuddy from '../assets/gym.png'
+import knowledge from '../assets/knowledge.png'
 const Websites = () => {
     return (
-        <>
+        <div>
             <h2> Websites</h2>
             <div className="heroBanner" style={{ display: 'flex' }}>
 
                 <a href="https://myexercisebuddy.netlify.app/" target="_blank">
-                    <img src={exerciseBuddy} alt="exerciseBuddy" style={{ width: '500px' }} />
+                    <img src={exerciseBuddy} alt="My Exercise Buddy" style={{ width: '500px' }} />
                 </a>
                 <div className='center-align'>
                     <h3>My Exercise Buddy</h3>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+                        <button className="language-buttons">React</button>
+                        <button className="language-buttons">MUI</button>
+                        <button className="language-buttons">rapidapi</button>
+                    </div>
                     <p style={{ padding: '0 40px' }}>An app that lets you search for workouts. You can target muscle group or equipments. Each workout includes a video and related workouts</p>
                 </div>
             </div >
-        </>
+            <div className="heroBanner" style={{ display: 'flex' }}>
+                <div className='center-align'>
+                    <h3>Knowledge center - Liveperson</h3>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+                        <button className="language-buttons">React</button>
+                        <button className="language-buttons">Tailwaind</button>
+                        <button className="language-buttons">Graphql</button>
+                        <button className="language-buttons">Express</button>
+                        <button className="language-buttons">NodeJs</button>
+                    </div>
+                    <p style={{ padding: '0 40px' }}>Liveperson  knowledge center site. Helps users understand how they can use liverspon converstational cloud  </p>
+
+                </div>
+                <a href="https://knowledge.liveperson.com/" target="_blank">
+                    <img src={knowledge} alt="Knowledge center - Liveperson" style={{ width: '500px' }} />
+                </a>
+            </div >
+
+        </div>
     )
 }
 
