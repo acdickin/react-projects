6b75645f8b0494082e238cc750e998b61bb68acc
Andrew Cosmic <acdickin@gmail.com>
2024-05-26 14:39:22 -0700

    added chat app

--- src/components/Websites.tsx
+++ src/components/Websites.tsx
@@ -1,10 +1,11 @@
 import exerciseBuddy from '../assets/gym.png'
 import knowledge from '../assets/knowledge.png'
+import chat from '../assets/chatapp.png'
 const Websites = () => {
     return (
         <div>
             <h2> Websites</h2>
-            <div className="heroBanner">
+            <div className="heroBanner ">
                 <a href="https://myexercisebuddy.netlify.app/" target="_blank">
                     <img src={exerciseBuddy} alt="My Exercise Buddy" style={{ width: '500px' }} />
                 </a>
@@ -18,7 +19,28 @@ const Websites = () => {
                     <p style={{ padding: '0 40px' }}>An app that lets you search for workouts. You can target muscle groups or equipment. Each workout includes a video and related workouts</p>
                 </div>
             </div >
+
+            <hr style={{ width: "80%", marginBottom: "32px" }} />
+
             <div className="heroBanner reverse" >
+                <a href="https://chat-project-qdn7.onrender.com/login" target="_blank">
+                    <img src={chat} alt="Chat App" style={{ width: '500px' }} />
+                </a>
+                <div className='center-align '>
+                    <h3>Chat app</h3>
+                    <div className='websiteButtons'>
+                        <button className="language-buttons">MERN</button>
+                        <button className="language-buttons">socket.Io</button>
+                        <button className="language-buttons">JWT</button>
+                        <button className="language-buttons">Tailwind</button>
+                    </div>
+                    <p style={{ padding: '0 40px' }}>Liveperson  knowledge center site. Helps users understand how they can use Liveperson Conversational Cloud  </p>
+                </div>
+
+            </div >
+
+            <hr style={{ width: "80%", marginBottom: "32px" }} />
+            <div className="heroBanner" >
                 <a href="https://knowledge.liveperson.com/" target="_blank">
                     <img src={knowledge} alt="Knowledge center - Liveperson" style={{ width: '500px' }} />
                 </a>
@@ -32,11 +54,8 @@ const Websites = () => {
                         <button className="language-buttons">NodeJs</button>
                     </div>
                     <p style={{ padding: '0 40px' }}>Liveperson  knowledge center site. Helps users understand how they can use Liveperson Conversational Cloud  </p>
-
                 </div>
-
             </div >
-
         </div >
     )
 }
