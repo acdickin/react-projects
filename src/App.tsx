8686bb0c5e7279c86858fc5ab2fcc7d8137f4753
Andrew Cosmic <acdickin@gmail.com>
2026-08-05 11:19:09 -0700

    seo optimization

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Layout from "./layout/Layout";
+import Seo from "./components/Seo";
 
 function App() {
   const clientQuery = new QueryClient();
   return (
     <QueryClientProvider client={clientQuery}>
+      <Seo />
       <main>
         <Layout>
           <Outlet />
